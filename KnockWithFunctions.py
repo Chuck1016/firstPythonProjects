864a0950bb47792ab963f45ecf4083a323595180
Chuck1016 <50124862+Chuck1016@users.noreply.github.com>
2020-02-27 16:19:08 -0500

    Update KnockWithFunctions.py
    Added .lower() to answers to remove case errors

--- KnockWithFunctions.py
+++ KnockWithFunctions.py
@@ -1,20 +1,20 @@
  
 def Banana():
     answer1 = ''
-    while answer1 != "who's there":
+    while answer1.lower() != "who's there":
         print('Knock knock')
         answer1 = input()
     answer2 = ''
-    while answer2 != "Banana who":
+    while answer2.lower() != "banana who":
         print('Banana')
         answer2 = input()
 def Orange():
     answer3 = ''
-    while answer3 != "who's there":
+    while answer3.lower() != "who's there":
         print ("knock knock")
         answer3 = input()
         answer4 = ''    
-    while answer4 != "Orange who":
+    while answer4.lower() != "orange who":
         print('Orange')
         answer4 = input()
 
